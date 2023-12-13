144139fc244b0c90160ea1a4ff92666f2cd852dc
segunda-chance <ods3grupo1generation@gmail.com>
2023-12-13 11:02:17 -0300

    first commit

---
+++ src/main/java/com/generation/segundachance/SegundachanceApplication.java
@@ -0,0 +1,13 @@
+package com.generation.segundachance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundachanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundachanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/segundachance/SegundachanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.segundachance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundachanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
