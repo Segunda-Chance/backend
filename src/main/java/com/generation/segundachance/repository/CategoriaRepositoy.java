79c74b7a8ce47ad026ebfe7a9b7b92c467016839
Maria <m.leiliane.lobo@gmail.com>
2023-12-15 09:59:17 -0300

    interface categoria criada

---
+++ src/main/java/com/generation/segundachance/repository/CategoriaRepositoy.java
@@ -0,0 +1,9 @@
+package com.generation.segundachance.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.segundachance.model.Categoria;
+
+public interface CategoriaRepositoy extends JpaRepository<Categoria, Long>{
+
+}
