339c36ce5d4d6eef12142adfc92e5d916968989f
LuizEduardoSC <luizeduardoedd1@gmail.com>
2023-12-15 11:52:09 -0300

    Implemnentação metodos Put e Post

--- src/main/java/com/generation/segundachance/controller/CategoriaController.java
+++ src/main/java/com/generation/segundachance/controller/CategoriaController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +20,8 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.segundachance.model.Categoria;
 import com.generation.segundachance.repository.CategoriaRepository;
+
+import jakarta.validation.Valid;
 
 
 @RestController
@@ -37,22 +42,19 @@ public class CategoriaController {
 		return ResponseEntity.ok(categoriaRepository.findAllByNomeCategoriaContainingIgnoreCase(nomeCategoria));
 	}
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+	@PostMapping
+    public ResponseEntity<Categoria> post(@Valid @RequestBody Categoria categoria){
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(categoriaRepository.save(categoria));
+    }
+
+    @PutMapping
+    public ResponseEntity<Categoria> put(@Valid @RequestBody Categoria categoria){
+        return categoriaRepository.findById(categoria.getId())
+            .map(resposta -> ResponseEntity.status(HttpStatus.CREATED)
+            .body(categoriaRepository.save(categoria)))
+            .orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    }
 	
 	
 	@DeleteMapping("/{id}")
